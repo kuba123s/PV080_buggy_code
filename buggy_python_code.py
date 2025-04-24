10043e5b2bf2fe467ee89faa43d82eb553cde167
unknown <xsimandl@ad.fi.muni.cz>
2025-04-24 11:01:33 +0200

    code injection repair

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -57,6 +57,8 @@ if __name__ == '__main__':
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
+        if (urlib_version !== "2"):
+            urlib_version = "3"
         fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
         load_yaml(input("File name: "))
